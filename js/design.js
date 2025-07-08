93553a6b11143689aafad2a55596b66063c7b06a
venky348 <venkateshtalasila348@gmail.com>
2025-07-08 09:17:48 -0400

    used the same svg file for all screen sizes

--- js/design.js
+++ js/design.js
@@ -9,7 +9,15 @@ $(document).ready(function(){
             var newNavHeight = $('nav > ul').height() + 15;
             $('nav').animate({'height':newNavHeight + 'px'}, 750);
         }else{
-            $('nav').animate({'height':'0px'}, 750);
+            $('nav').animate({'height':'0px'}, 750, function(){
+                $(this).removeAttr('style');
+            });
+        }
+    });
+
+    $(window).resize(function(){
+        if ($(this).width() > 625){
+            $('nav').removeAttr('style');
         }
     });
 
